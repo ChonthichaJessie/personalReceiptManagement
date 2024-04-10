ce546bd9ad606b3988bb222a5a4048ba6e057392
Jessie <chonthicha.pai-ngam@edu.sait.ca>
2024-04-09 19:54:37 -0600

    hide firestore keys

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+//import axios from "axios";
 import { useEffect } from "react";
 import { initializeApp } from "firebase/app";
 import {
@@ -20,20 +20,9 @@ import AlgoliaSearchLists from "./components/algoliaSearchLists";
 import UploadImage from "./components/uploadImages";
 import OcrDisplay from "./components/ocrDisplay";
 import ImagesDisplay from "./components/imagesDisplay";
+import { db } from "./utils/firebase";
 
 const { ALGOLIA_APP_ID, ALGOLIA_ADMIN_KEY, ALGOLIA_INDEX_NAME } = algoliaConfig;
-
-//Firebase Key - - Need to hide the keys
-const firebaseConfig = {
-  apiKey: "AIzaSyD3RtJUUsBuocFfrXst0QJpbGfG2LTAz2Y",
-  authDomain: "receiptmanagement-1d51d.firebaseapp.com",
-  projectId: "receiptmanagement-1d51d",
-  storageBucket: "receiptmanagement-1d51d.appspot.com",
-  messagingSenderId: "852913556492",
-  appId: "1:852913556492:web:6b65c58bfec0992ac47229",
-};
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
 const App = () => {
   const [jsonData, setJsonData] = useState(null);
@@ -41,38 +30,40 @@ const App = () => {
   const [receipt, setReceipt] = useState(receiptData);
   // Algolia
   const [searchClient, setSearchClient] = useState(null);
+
+  // const ocrFetching = async () => {
+  //   try {
+  //     let data = JSON.stringify({
+  //       file_url:
+  //         "https://downloads.intercomcdn.com/i/o/531448844/04b5b529d5578dd603b48dee/cpg-receipt-grocery.jpg?expires=1711134631&signature=77a56224c6640186616ff6cb66a496393d3e76ea30b4f3e86525200ba4cf240f",
+  //     });
+
+  //     let config = {
+  //       method: "post",
+  //       maxBodyLength: Infinity,
+  //       url: "https://cors-anywhere.herokuapp.com/https://api.veryfi.com/api/v8/partner/documents",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         Accept: "application/json",
+  //         "CLIENT-ID": "vrfWxnyNHLND0bWymAZBlTbWp6gzlfoPEdddlaj",
+  //         AUTHORIZATION:
+  //           "apikey chonthicha.pc:f3da01f277e17bc6bec576c280d1452f",
+  //       },
+  //       data: data,
+  //     };
+
+  //     const response = await axios(config);
+  //     setJsonData(response.data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
   const [uploadedImageURLs, setUploadedImagesURLs] = useState([]);
-  
+
   const handleImageUpload = (imageURLs) => {
     setUploadedImagesURLs(imageURLs);
-  };
-
-  const ocrFetching = async () => {
-    try {
-      let data = JSON.stringify({
-        file_url:
-          "https://downloads.intercomcdn.com/i/o/531448844/04b5b529d5578dd603b48dee/cpg-receipt-grocery.jpg?expires=1711134631&signature=77a56224c6640186616ff6cb66a496393d3e76ea30b4f3e86525200ba4cf240f",
-      });
-
-      let config = {
-        method: "post",
-        maxBodyLength: Infinity,
-        url: "https://cors-anywhere.herokuapp.com/https://api.veryfi.com/api/v8/partner/documents",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-          "CLIENT-ID": "vrfWxnyNHLND0bWymAZBlTbWp6gzlfoPEdddlaj",
-          AUTHORIZATION:
-            "apikey chonthicha.pc:f3da01f277e17bc6bec576c280d1452f",
-        },
-        data: data,
-      };
-
-      const response = await axios(config);
-      setJsonData(response.data);
-    } catch (error) {
-      console.log(error);
-    }
+    //call ocr from here
+    //ocrFetching();
   };
 
   const addItemToFirestore = async () => {
@@ -88,11 +79,9 @@ const App = () => {
     }
   };
 
-  const uploadReceiptHandler = () => {
-    ocrFetching();
-    addItemToFirestore();
-    console.log("Upload receipt");
-  };
+  // const uploadReceiptHandler = () => {
+  //   addItemToFirestore();
+  // };
 
   useEffect(() => {
     // const fetchData = async () => {
@@ -134,10 +123,9 @@ const App = () => {
       <h1>Receipt Management</h1>
       <UploadImage onImageUpload={handleImageUpload} />
       <Display>
-      <ImagesDisplay imageURLs={uploadedImageURLs} />
-      <OcrDisplay data={JSON.stringify(receipt, null, 1)} />
+        <ImagesDisplay imageURLs={uploadedImageURLs} />
+        <OcrDisplay data={JSON.stringify(receipt, null, 1)} />
       </Display>
-      
       <button onClick={addItemToFirestore}>Upload receipt</button>
       {searchClient && (
         <AlgoliaSearchLists
