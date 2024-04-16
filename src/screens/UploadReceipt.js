bd4ac90ebb7ef80a2828b0ab2f2b4f63ae6b5101
Jessie <chonthicha.pai-ngam@edu.sait.ca>
2024-04-16 15:38:49 -0600

    connect with ocr again

--- src/screens/UploadReceipt.js
+++ src/screens/UploadReceipt.js
@@ -1,5 +1,5 @@
 import React, { useMemo, useState } from "react";
-//import axios from "axios";
+import axios from "axios";
 import { useEffect } from "react";
 import { setDoc, doc } from "firebase/firestore";
 import { db, storage } from "../utils/firebase";
@@ -14,23 +14,21 @@ import {
 import ImagePicker from "../components/ImagePicker";
 import OcrDisplay from "../components/OcrDisplay";
 import ImagesDisplay from "../components/ImagesDisplay";
-import {ocrVeryfiConfig} from "../utils/ocrVeryfi";
+import { ocrVeryfiConfig } from "../utils/ocrVeryfi";
 
-const UploadReceipts = ({userEmail}) => {
+const UploadReceipts = ({ userEmail }) => {
   const [jsonData, setJsonData] = useState(null);
   const [receipt, setReceipt] = useState(receiptData);
   const [uid, setUid] = useState("");
   const [localImages, setLocalImages] = useState([]);
   const [fireImageURLs, setFireImageURLs] = useState(null);
 
-
-
   // const ocrFetching = async () => {
   //   const { clientID, apiKey } = ocrVeryfiConfig;
+  //   console.log("fireImageURLs", fireImageURLs[0]);
   //   try {
   //     let data = JSON.stringify({
-  //       file_url: fireImageURLs[0],
-  //         // "https://downloads.intercomcdn.com/i/o/531448844/04b5b529d5578dd603b48dee/cpg-receipt-grocery.jpg?expires=1711134631&signature=77a56224c6640186616ff6cb66a496393d3e76ea30b4f3e86525200ba4cf240f",
+  //       file_url:"https://downloads.intercomcdn.com/i/o/531448844/04b5b529d5578dd603b48dee/cpg-receipt-grocery.jpg?expires=1711134631&signature=77a56224c6640186616ff6cb66a496393d3e76ea30b4f3e86525200ba4cf240f",
   //     });
 
   //     let config = {
@@ -41,9 +39,9 @@ const UploadReceipts = ({userEmail}) => {
   //       headers: {
   //         "Content-Type": "application/json",
   //         Accept: "application/json",
-  //         "CLIENT-ID": clientID,
+  //         "CLIENT-ID": "vrfWxnyNHLND0bWymAZBlTbWp6gzlfoPEdddlaj",
   //         AUTHORIZATION:
-  //           apiKey,
+  //           "apikey chonthicha.pc:f3da01f277e17bc6bec576c280d1452f",
   //       },
   //       data: data,
   //     };
@@ -55,12 +53,41 @@ const UploadReceipts = ({userEmail}) => {
   //   }
   // };
 
-  
+  const fetchData = async () => {
+    try {
+      let data = JSON.stringify({
+        // file_url:
+        //   "https://downloads.intercomcdn.com/i/o/531448844/04b5b529d5578dd603b48dee/cpg-receipt-grocery.jpg?expires=1711134631&signature=77a56224c6640186616ff6cb66a496393d3e76ea30b4f3e86525200ba4cf240f",
+        file_url: fireImageURLs[0],
+      });
+
+      let config = {
+        method: "post",
+        maxBodyLength: Infinity,
+        url: "https://cors-anywhere.herokuapp.com/https://api.veryfi.com/api/v8/partner/documents",
+        //Hide all of the keys and tokens
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          "CLIENT-ID": "vrfWxnyNHLND0bWymAZBlTbWp6gzlfoPEdddlaj",
+          AUTHORIZATION:
+            "apikey chonthicha.pc:f3da01f277e17bc6bec576c280d1452f",
+        },
+        data: data,
+      };
+
+      const response = await axios(config);
+      //console.log(JSON.stringify(response.data));
+      setJsonData(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const receiptDateJoinID =
     JSON.stringify(receipt.date) + JSON.stringify(receipt.id) + Math.random();
   const receiptNumber = receiptDateJoinID.replace(/[^a-zA-Z0-9]/g, "");
 
- 
   const addImagesToFirestore = async () => {
     try {
       const imageURLs = await Promise.all(
@@ -100,9 +127,14 @@ const UploadReceipts = ({userEmail}) => {
     }
   };
 
-  const uploadReceiptHandler = async () => {
+  const convertReceiptToText = async () => {
     await addImagesToFirestore();
     console.log("Images uploaded to storage", fireImageURLs);
+    await fetchData();
+    setReceipt(jsonData);
+  };
+
+  const uploadReceiptHandler = async () => {
     await addItemToFirestore();
   };
 
@@ -149,8 +181,9 @@ const UploadReceipts = ({userEmail}) => {
         <ImagesDisplay
           imageURLs={localImages.map((image) => URL.createObjectURL(image))}
         />
-        <OcrDisplay data={JSON.stringify(receipt, null, 1)} />
+        <OcrDisplay data={JSON.stringify(jsonData, null, 1)} />
       </Display>
+      <button onClick={convertReceiptToText}>Convert receipt to text</button>
       <button onClick={uploadReceiptHandler}>Upload receipt</button>
       {/* {jsonData && (
         <div>
