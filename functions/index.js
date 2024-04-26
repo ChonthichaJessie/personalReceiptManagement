1a950dc7eea9396cd9ef700e6fe9bd7769ef81d5
Jessie <chonthicha.pai-ngam@edu.sait.ca>
2024-04-26 11:49:43 -0600

    cloud function update Access-Control-Allow-Origin

--- functions/index.js
+++ functions/index.js
@@ -9,44 +9,61 @@ console.log(ocrVeryfiConfig);
 
 const {onRequest} = require("firebase-functions/v2/https");
 
-exports.performOcr = onRequest(async (request, response) => {
-  try {
-    const imageURL = request.body.imageURL;
+// eslint-disable-next-line max-len
+exports.performOcr = onRequest(
+    async (request, response) => {
+      try {
+        const imageURL = request.body.imageURL;
 
-    const data = {
-      file_url: imageURL,
-    };
+        if (request.method === "OPTIONS") {
+          response.header("Access-Control-Allow-Origin", "https://personal-receipt-management.vercel.app");
+          response.header("Access-Control-Allow-Credentials", "true");
+          response.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+          response.header("Access-Control-Allow-Headers", "Content-Type");
+          response.end();
+          return;
+        }
 
-    if (!imageURL) {
-      throw new Error("Image URL is missing in the request body");
-    }
+        const data = {
+          file_url: imageURL,
+        };
 
-    const config = {
-      method: "post",
-      maxBodyLength: Infinity,
-      url: "https://api.veryfi.com/api/v8/partner/documents",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-        "CLIENT-ID": "vrfWxnyNHLND0bWymAZBlTbWp6gzlfoPEdddlaj",
-        // eslint-disable-next-line max-len
-        "AUTHORIZATION": "apikey chonthicha.pc:f3da01f277e17bc6bec576c280d1452f",
-        // Fix CORs issue for local host 3000
-        "Accss-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Credentials": "true",
-        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type",
-      },
-      data: data,
-    };
+        if (!imageURL) {
+          throw new Error("Image URL is missing in the request body");
+        }
 
-    const veryfiResponse = await axios(config);
-    response.send(veryfiResponse.data ?? {});
-  } catch (error) {
-    console.error("Error in performOcr function:", error);
-    console.log(error);
-    response.send({
-      error: "Error in OCR Veryfi API call-indirect OCR Key" + error.message,
-    });
-  }
-});
+        const config = {
+          method: "post",
+          maxBodyLength: Infinity,
+          url: "https://api.veryfi.com/api/v8/partner/documents",
+          headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "CLIENT-ID": process.env.REACT_APP_VERYFI_CLIENT_ID,
+            // eslint-disable-next-line max-len
+            "AUTHORIZATION":
+            process.env.REACT_APP_VERYFI_API_KEY,
+          },
+          data: data,
+        };
+
+        const veryfiResponse = await axios(config);
+        response.header("Access-Control-Allow-Origin", "https://personal-receipt-management.vercel.app");
+        response.header("Access-Control-Allow-Credentials", "true");
+        response.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+        response.header("Access-Control-Allow-Headers", "Content-Type");
+        response.status(200).send(veryfiResponse.data ?? {});
+      } catch (error) {
+        console.error("Error in performOcr function:", error);
+        console.log(error);
+        response.header("Access-Control-Allow-Origin", "https://personal-receipt-management.vercel.app");
+        response.header("Access-Control-Allow-Credentials", "true");
+        response.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+        response.header("Access-Control-Allow-Headers", "Content-Type");
+        response.send({
+          // eslint-disable-next-line max-len
+          error: "Error in OCR Veryfi API call-indirect OCR Key" + error.message,
+        });
+      }
+    },
+);
